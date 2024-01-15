1b92b223ba9c2e8293d4231f9dc2116a980e7137
Nikita Romanov <nr75295@gmail.com>
2024-01-15 19:35:05 +0300

    Lesson3fixed

--- Lesson 3/Task2/main.cpp
+++ Lesson 3/Task2/main.cpp
@@ -4,44 +4,48 @@ class Counter {
 private:
 	int counter;
 public:
+	Counter() {
+		this->counter = 1;
+	}
 	Counter(int counter) {
 		this->counter = counter;
 	}
-	void changer(std::string status) {
-		if (status == "+") {
-			this->counter += 1;
-		}
-		else if (status == "-") {
-			this->counter -= 1;
-		}
-		else if (status == "=") {
-			std::cout << this->counter << "\n";
-		}
+	void increase() {
+		this->counter += 1;
+	}
+	void decrease() {
+		this->counter -= 1;
+	}
+	int output() {
+		 return(this->counter);
 	}
 };
 int main(int argc, char** argv) {
 	setlocale(LC_ALL, "Russian");
-	int counter = 1;
-	Counter c(counter);
+	int counter = 0;
 	std::string status;
-	std::string da;
+	std::string da; 
 	std::cout << "Вы хотите указать начальное значение счётчика? Введите y или n: ";
 	std::cin >> da;
 	if (da == "y") {
 		std::cout << "Введите начальное значение счётчика: ";
 		std::cin >> counter;
 	}
-	Counter g(counter);
+	else if (da == "n") {
+		counter = 1;
+	}
 	do {
 		std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
 		std::cin >> status;
-		if (status == "+" || status == "-" || status == "=" || status == "x") {
-			if (da == "y") {
-				g.changer(status);
-			}
-			else {
-				c.changer(status);
-			}
+		Counter g(counter);
+		if (status == "+") {
+			g.increase();
+		}
+		else if (status == "-") {
+			g.decrease();
+		}
+		else if (status == "=") {
+			std::cout<<g.output()<<"\n";
 		}
 		else {
 			std::cout << "Неверный ввод! \n";
