855ffa2e1b736ba427182706e1e482424fa7f086
Nikita Romanov <nr75295@gmail.com>
2024-01-17 18:39:55 +0300

    Lesson7f

--- Lesson 7/Task 1/main.cpp
+++ Lesson 7/Task 1/main.cpp
@@ -3,31 +3,33 @@
 #include "numresult.h"
 
 #define MODE 2
-#if !defined MODE
-#error "Не задан режим, необходимо установить MODE"
-#endif 
 
+#if !defined MODE || (MODE != 1 && MODE != 2)
+#error "Необходимо установить MODE в 1 или 2"
+#endif
 int main(int argc, char** argv) {
     setlocale(LC_ALL, "Russian");
+
 #ifdef MODE
 #if MODE == 1
-    std::cout << "Работаю в боевом режиме \n";
+    std::cout << "Работа в тестовом режиме\n";
 #elif MODE == 2
-    std::cout << "Работаю в тестовом режиме \n";
+    std::cout << "Работа в боевом режиме\n";
     int num1 = 0;
     int num2 = 0;
     int count = 1;
-    bool numMode = true;
     num1 = numcin::numcn(num1, count);
     ++count;
     num2 = numcin::numcn(num2, count);
-    std::cout<< "Результат сложения: " << numresult::numres(num1, num2) << std::endl;
-#else MODE
-    std::cout << "Неизвестный режим. Завершение работы \n";
-#endif 
+    std::cout << "Результат сложения: " << numresult::numres(num1, num2) << std::endl;
+#else
+    std::cout << "Неизвестный режим. Завершение программы\n";
 #endif
+#endif
+
 #ifndef MODE
-    std::cout << std::endl << "Не определен #error и не указан #define MODE. \n";
+    std::cout << std::endl << "Не определена константа MODE. \n";
 #endif
+
     return 0;
 }
