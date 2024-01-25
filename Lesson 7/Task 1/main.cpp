ea37cf48e325dc9db5ce60f34bd1fa5d4c66c5eb
Nikita Romanov <nr75295@gmail.com>
2024-01-25 17:35:33 +0300

    Lesson7_1fixed

--- Lesson 7/Task 1/main.cpp
+++ Lesson 7/Task 1/main.cpp
@@ -4,16 +4,16 @@
 
 #define MODE 1
 
-#if !defined MODE || (MODE != 1 && MODE != 2)
-#error "Необходимо установить MODE в 1 или 2"
+#if !defined MODE
+#error "Необходимо установить MODE"
 #endif
 int main(int argc, char** argv) {
     setlocale(LC_ALL, "Russian");
 
 #ifdef MODE
-#if MODE == 1
+#if MODE == 0
     std::cout << "Работа в тестовом режиме\n";
-#elif MODE == 2
+#elif MODE == 1
     std::cout << "Работа в боевом режиме\n";
     int num1 = 0;
     int num2 = 0;
@@ -26,10 +26,5 @@ int main(int argc, char** argv) {
     std::cout << "Неизвестный режим. Завершение программы\n";
 #endif
 #endif
-
-#ifndef MODE
-    std::cout << std::endl << "Не определена константа MODE. \n";
-#endif
-
     return 0;
 }
