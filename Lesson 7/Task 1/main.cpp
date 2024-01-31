c04df22538c646c75eb8f75d33da994b84ff4e0c
Nikita Romanov <nr75295@gmail.com>
2024-01-31 19:45:21 +0300

    Lesson7_1fixed2

--- Lesson 7/Task 1/main.cpp
+++ Lesson 7/Task 1/main.cpp
@@ -1,29 +1,40 @@
 #include <iostream>
-#include "numcin.h"
-#include "numresult.h"
 
-#define MODE 1
-
-#if !defined MODE
+#define MODE 0
+#ifndef MODE
 #error "Необходимо установить MODE"
 #endif
-int main(int argc, char** argv) {
+#if MODE == 0
+void trainingMode() {
+    std::cout << "Работаю в режиме тренировки\n";
+}
+#elif MODE == 1
+int add(int a, int b) {
+    return a + b;
+}
+void battleMode() {
+    std::cout << "Работаю в боевом режиме\n";
+}
+#else
+void unknownMode() {
+    std::cout << "Неизвестный режим. Завершение работы\n";
+}
+#endif
+int main() {
     setlocale(LC_ALL, "Russian");
-
 #ifdef MODE
 #if MODE == 0
-    std::cout << "Работа в тестовом режиме\n";
+    trainingMode();  
 #elif MODE == 1
-    std::cout << "Работа в боевом режиме\n";
-    int num1 = 0;
-    int num2 = 0;
-    int count = 1;
-    num1 = numcin::numcn(num1, count);
-    ++count;
-    num2 = numcin::numcn(num2, count);
-    std::cout << "Результат сложения: " << numresult::numres(num1, num2) << std::endl;
+    battleMode();
+    int num1, num2;
+    std::cout << "Введите первое число: ";
+    std::cin >> num1;
+    std::cout << "Введите второе число: ";
+    std::cin >> num2;
+    std::cout << "Результат сложения: " << add(num1, num2) << std::endl;
 #else
-    std::cout << "Неизвестный режим. Завершение программы\n";
+    unknownMode();
 #endif
 #endif
     return 0;
