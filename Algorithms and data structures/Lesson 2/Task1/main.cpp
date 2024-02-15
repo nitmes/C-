7e38614d84e348663d86115c671bab66ba08cf33
Nikita Romanov <nr75295@gmail.com>
2024-02-16 02:50:38 +0300

    Alghoritmil2f

--- Algorithms and data structures/Lesson 2/Task1/main.cpp
+++ Algorithms and data structures/Lesson 2/Task1/main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 
-int main() {
-    setlocale(LC_ALL, "Russian");
-    int n = 0;
-    std::cout << "Введите число: ";
-    std::cin >> n;
-    if (n < 1) {
-        std::cout << "Число должно быть больше 0. \n";
-        return 1;
-    }
-    int* fabo = new int[n + 1];
-    fabo[0] = 0;
-    if (n > 0) {
-        fabo[1] = 1;
-    }
-    std::cout << "Числа Фибоначчи: ";
-    for (int i = 2; i <= n; ++i) { 
-        fabo[i] = fabo[i - 1] + fabo[i - 2];
-    }
-    std::cout << fabo[n];
-    delete[] fabo;
-    return 0;
+int febo(int i) {
+	if (i == 1) {
+		return 0;
+	}
+	else if (i == 2) {
+		return 1;
+	}
+	return febo(i - 1) + febo(i - 2);
+}
+
+int main(int argc, char** argv) {
+	setlocale(LC_ALL, "Russian");
+	int n = 0;
+	std::cout << "Введите число:";
+	std::cin >> n;
+	std::cout << "Числа Фибоначчи: ";
+	for (int i = 1; i <= n; ++i) {
+		std::cout << febo(i) << " ";
+	}
+	return 0;
 }
---
+++ Algorithms and data structures/Lesson 2/task2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+    setlocale(LC_ALL, "Russian");
+    int n = 0;
+    std::cout << "Введите число: ";
+    std::cin >> n;
+    if (n < 1) {
+        std::cout << "Число должно быть больше 0. \n";
+        return 1;
+    }
+    int* fabo = new int[n + 1];
+    fabo[0] = 0;
+    if (n > 0) {
+        fabo[1] = 1;
+    }
+    std::cout << "Числа Фибоначчи: ";
+    for (int i = 2; i <= n; ++i) {
+        fabo[i] = fabo[i - 1] + fabo[i - 2];
+    }
+    std::cout << fabo[n];
+    delete[] fabo;
+    return 0;
+}
