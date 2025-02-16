6fe73dfba8cb2b0b677c2cbde017e5b2a858dfdd
Nikita Romanov <nr75295@gmail.com>
2025-02-16 04:36:18 +0300

    UE5

---
+++ UE5/Lesson 1 Blueprints P1/CppBaseActor.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CppBaseActor.h"
+#include "Math/UnrealMathUtility.h"
+// Sets default values
+ACppBaseActor::ACppBaseActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	EnemyNum = FMath::RandRange(0, 20);
+	IsAlive = FMath::RandBool();
+}
+
+// Called when the game starts or when spawned
+void ACppBaseActor::BeginPlay()
+{
+	Super::BeginPlay();
+	ShowInformation();
+}
+
+// Called every frame
+void ACppBaseActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+void ACppBaseActor::ShowInformation() 
+{
+	FString IntanceName = GetName();
+
+	UE_LOG(LogTemp, Display, TEXT("IntanceName: %s"), *IntanceName);
+	UE_LOG(LogTemp, Display, TEXT("EnemyNum: %d"), EnemyNum);
+	UE_LOG(LogTemp, Display, TEXT("IsAlive: %i"), IsAlive);
+}
+
+
