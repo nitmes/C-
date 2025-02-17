a720d8bb3ad0c0db3c292864d4ddc7422d8ad21c
Nikita Romanov <nr75295@gmail.com>
2025-02-17 16:50:50 +0300

    UE51fix

--- UE5/Lesson 1 Blueprints P1/CppBaseActor.cpp
+++ UE5/Lesson 1 Blueprints P1/CppBaseActor.cpp
@@ -8,8 +8,6 @@ ACppBaseActor::ACppBaseActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	EnemyNum = FMath::RandRange(0, 20);
-	IsAlive = FMath::RandBool();
 }
 
 // Called when the game starts or when spawned
